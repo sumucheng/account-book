3a71a9b35b79d964a10e149bd1789e184c32fbc4
kiki <772765729@qq.com>
2020-03-02 20:20:12 +0800

    deploy

---
+++ precache-manifest.1290f6d700b3e1c49ca53ae3e6bb6167.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c25b1fd0fd26b3dd19fb",
+    "url": "/account-book/css/app.de232769.css"
+  },
+  {
+    "revision": "1cbe32c667653584b48b1c575a76db34",
+    "url": "/account-book/img/background.1cbe32c6.png"
+  },
+  {
+    "revision": "baaf899b08e64dffab1f1bbe586afb6a",
+    "url": "/account-book/index.html"
+  },
+  {
+    "revision": "c25b1fd0fd26b3dd19fb",
+    "url": "/account-book/js/app.b7173c9b.js"
+  },
+  {
+    "revision": "ba371fe20c74db610914",
+    "url": "/account-book/js/chunk-vendors.304320a6.js"
+  },
+  {
+    "revision": "d0f5904f2f26295f953211564e3ca0a7",
+    "url": "/account-book/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/account-book/robots.txt"
+  }
+]);
